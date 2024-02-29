c06095cebee50c901451fd71227c0c5afbd5bebd
lagsmana13 <lagsmana@yahoo.com>
2024-02-29 02:04:54 -0800

    2x

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,30 +1,25 @@
 #include "binary_trees.h"
 
 /**
- * create_binary_tree_node - Creates a new binary tree node.
- * @parent: A pointer to the parent node.
- * @value: The value to be stored in the new node.
+ * binary_tree_node - Creates a binary tree node.
+ * @parent: A pointer to the parent of the node to create.
+ * @value: The value to put in the new node.
  *
- * Return: If memory allocation fails, returns NULL.
- *         Otherwise, returns a pointer to the newly created node.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
  */
-binary_tree_t *create_binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new;
 
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
 
-    // Assign the value to the new node
-    new_node->n = value;
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 
-    // Set the parent of the new node
-    new_node->parent = parent;
-
-    // Set the left and right children of the new node as NULL
-    new_node->left = NULL;
-    new_node->right = NULL;
-
-    return (new_node);
+	return (new);
 }
